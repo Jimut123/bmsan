2670dbfc043fb3c8be008f1deb053e4550681b3b
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-12-29 00:45:44 +0530

    brain mri done

--- BrainMRI/R2UNet/r2unet_brain_mri.py
+++ BrainMRI/R2UNet/r2unet_brain_mri.py
@@ -85,64 +85,6 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 Y_train = Y_train.reshape((Y_train.shape[0],Y_train.shape[1],Y_train.shape[2],1))
 Y_test = Y_test.reshape((Y_test.shape[0],Y_test.shape[1],Y_test.shape[2],1))
 
-X_train = X_train / 255
-X_test = X_test / 255
-Y_train = Y_train / 255
-Y_test = Y_test / 255
-
-Y_train = np.round(Y_train,0)
-Y_test = np.round(Y_test,0)
-
-print(X_train.shape)
-print(Y_train.shape)
-print(X_test.shape)
-print(Y_test.shape)
-
-
-from tqdm import tqdm
-img_files = glob.glob('original_img/*.tif')
-msk_files = glob.glob('ground_truth/*.tif')
-
-img_files.sort()
-msk_files.sort()
-
-print(len(img_files))
-print(len(msk_files))
-
-
-
-
-X = []
-Y = []
-
-for img_fl in tqdm(img_files):
-  #print(img_fl)
-  name = str(img_fl.split('.')[0]).split('/')[1]
-  original_name = "original_img/"+name+".tif"
-  #print(name)
-  mask_name = "ground_truth/"+name+"_mask.tif"
-  #break
-  if(img_fl.split('.')[-1]=='tif'):
-    img = cv2.imread('{}'.format(original_name), cv2.IMREAD_COLOR)
-    #resized_img = cv2.resize(img,(256, 256), interpolation = cv2.INTER_CUBIC)
-
-    X.append(img) #resized_img)
-
-    msk = cv2.imread('{}'.format(mask_name), cv2.IMREAD_GRAYSCALE)
-    #resized_msk = cv2.resize(msk,(256, 256), interpolation = cv2.INTER_CUBIC)
-
-    Y.append(msk)#resized_msk)
-
-print(len(X))
-print(len(Y))
-
-X = np.array(X)
-Y = np.array(Y)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=3)
-
-Y_train = Y_train.reshape((Y_train.shape[0],Y_train.shape[1],Y_train.shape[2],1))
-Y_test = Y_test.reshape((Y_test.shape[0],Y_test.shape[1],Y_test.shape[2],1))
 
 X_train = X_train / 255
 X_test = X_test / 255
