22684a4404394749a5a815ffcc180a877662373f
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-12-28 23:28:29 +0530

    1 epoch test

--- SKIN_LEISON/R2UNet/r2unet_skin_lesion.py
+++ SKIN_LEISON/R2UNet/r2unet_skin_lesion.py
@@ -285,5 +285,5 @@ fp = open('models/best_r2unet_skinlesion.txt','w')
 fp.write('-1.0')
 fp.close()
 
-trainStep(model, X_train, Y_train, X_test, Y_test, epochs=150, batchSize=2)
+trainStep(model, X_train, Y_train, X_test, Y_test, epochs=1, batchSize=2)
 
