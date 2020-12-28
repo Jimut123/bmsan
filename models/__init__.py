0c8cc3fb1a16c24bdc4be08e1a1ff9349fa1bf6c
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-12-28 19:29:07 +0530

    some

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@
 #from .jimutmap import api
 from .MultiResUNet import MultiResUnet
 from .DRRMSAN import DRRMSAN_multiscale_attention
-from UNet_AttnUNet_R2UNet_AttnR2UNet import unet, att_unet, r2_unet, att_r2_unet
+from .UNet_AttnUNet_R2UNet_AttnR2UNet import unet, att_unet, r2_unet, att_r2_unet
 
