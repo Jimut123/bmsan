b989bb82602abe8f52e4242aef476637aee91672
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2020-12-28 23:18:52 +0530

    modified UNET

--- ISIC_2017/MultiResUNet/isic_multiResUNet.py
+++ ISIC_2017/MultiResUNet/isic_multiResUNet.py
@@ -234,6 +234,6 @@ fp = open('models/best_multi_res_unet_isic.txt','w')
 fp.write('-1.0')
 fp.close()
 
-trainStep(model, X_train, Y_train, X_test, Y_test, epochs=150, batchSize=2)
+trainStep(model, X_train, Y_train, X_test, Y_test, epochs=1, batchSize=2)
 
 
