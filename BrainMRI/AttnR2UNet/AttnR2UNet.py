f007b9f1a800eb4f449333d5ef3f268cf67aa934
Jimut <jimutbahanpal@yahoo.com>
2020-12-30 00:50:23 +0530

    some changes

--- BrainMRI/AttnR2UNet/AttnR2UNet.py
+++ BrainMRI/AttnR2UNet/AttnR2UNet.py
@@ -296,6 +296,6 @@ fp = open('models/best_attnR2Unet_brainMRI.txt','w')
 fp.write('-1.0')
 fp.close()
 
-trainStep(model, X_train, Y_train, X_test, Y_test, epochs=150, batchSize=2)
+trainStep(model, X_train, Y_train, X_test, Y_test, epochs=5, batchSize=2)
         
     
