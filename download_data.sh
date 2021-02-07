00994fbea7d9b696e3ea602ae158d27c715157d9
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-07 23:32:08 +0530

    some of the codes updated

---
+++ download_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# download all the dataset hosted from
+# my gsuite account
+
+# ISIC 2017 Datset
+gdown https://drive.google.com/u/3/uc?id=1-PAttyqJLImKMdz0gTLM3soiWWvi8PAD
+
+# BrainMRI Datset
+gdown https://drive.google.com/u/3/uc?id=1-YLWaVt4d1MA51Ljid4mznaDg5e1xafc
+
+# Skin Lesion Datset
+gdown https://drive.google.com/u/0/uc?id=1-OdmzC3JvmUsZUMrNAKPnu8ZM7EeSK04
+
+# Lungs Datset
+gdown https://drive.google.com/u/3/uc?id=1wkephfRzoFhrcRbz_7a97_muu8ecSJ9d
+
