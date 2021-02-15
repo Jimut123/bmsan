7fc98f5c74b31b2b5b577b1f5a2cd5b72b369f43
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-15 11:59:53 +0530

    dataset kvasir mirror added

--- download_data.sh
+++ download_data.sh
@@ -15,3 +15,6 @@ gdown https://drive.google.com/u/0/uc?id=1-OdmzC3JvmUsZUMrNAKPnu8ZM7EeSK04
 # Lungs Datset
 gdown https://drive.google.com/u/3/uc?id=1wkephfRzoFhrcRbz_7a97_muu8ecSJ9d
 
+# KVASIR-SEG Dataset
+gdown https://drive.google.com/u/3/uc?id=1VzOUEvIyhmOGQUBGzfI7M8GMW2NjIFeH
+
