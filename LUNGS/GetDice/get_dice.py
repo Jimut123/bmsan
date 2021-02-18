53043d7615ee2661d845f84d85e2209613653eac
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-18 11:01:57 +0530

    drrmsan lungs added

--- LUNGS/GetDice/get_dice.py
+++ LUNGS/GetDice/get_dice.py
@@ -8,7 +8,7 @@ for i in range(20):
     alpha_1 = float("%0.2f" % (alphas[0][0]))
     alpha_2 = float("%0.2f" % (alphas[0][1]))
     alpha_3 = float("%0.2f" % (alphas[0][2]))
-    alpha_4 = float("%0.2f" % (alphas[0][3])))
+    alpha_4 = float("%0.2f" % (alphas[0][3]))
 
     print(alpha_1," ",alpha_2," ",alpha_3," ",alpha_4," = ",alpha_1+alpha_2+alpha_3+alpha_4)
     dice = drrmsan_multilosses.get_dice_from_alphas(
