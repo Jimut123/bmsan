85f5bc85c5081d15605a408f220c708bed097bf3
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-19 10:30:30 +0530

    dice changes

--- LUNGS/GetDice/get_dice.py
+++ LUNGS/GetDice/get_dice.py
@@ -1,19 +1,21 @@
 import numpy as np, numpy.random
 import drrmsan_multilosses
+import pickle
 
 # Generating Cases
 
-for i in range(20):
-    alphas = np.random.dirichlet(np.ones(4),size=1)
-    alpha_1 = float("%0.2f" % (alphas[0][0]))
-    alpha_2 = float("%0.2f" % (alphas[0][1]))
-    alpha_3 = float("%0.2f" % (alphas[0][2]))
-    alpha_4 = float("%0.2f" % (alphas[0][3]))
 
+with open('alpha_datas.pickle', 'rb') as handle:
+    data = pickle.load(handle)
+
+for item in data:
+    alpha_1 = float("%0.2f" % (item[0][0]))
+    alpha_2 = float("%0.2f" % (item[0][1]))
+    alpha_3 = float("%0.2f" % (item[0][2]))
+    alpha_4 = float("%0.2f" % (item[0][3]))
     print(alpha_1," ",alpha_2," ",alpha_3," ",alpha_4," = ",alpha_1+alpha_2+alpha_3+alpha_4)
     dice = drrmsan_multilosses.get_dice_from_alphas(
         float(alpha_1), float(alpha_2), float(alpha_3), float(alpha_4))
-
 
 
 
