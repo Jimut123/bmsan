ceb98ec2ea16052e895dcddb883f13a14d0af867
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-28 18:36:05 +0530

    drrmsan added

--- SKIN_LEISON/drrmsan_tests/tests.py
+++ SKIN_LEISON/drrmsan_tests/tests.py
@@ -269,6 +269,6 @@ fp = open('models/best.txt','w')
 fp.write('-1.0')
 fp.close()
 
-trainStep(model, X_train, Y_train, X_test, Y_test, epochs=150, batchSize=2)
+trainStep(model, X_train, Y_train, X_test, Y_test, epochs=20, batchSize=2)
 
 
