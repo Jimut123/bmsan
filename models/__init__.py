1df00d5cedf6a438915a7ba97fe3e945753c17d9
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-02-28 18:31:53 +0530

    drrmsan added

--- models/__init__.py
+++ models/__init__.py
@@ -10,4 +10,5 @@ from .DRRMSAN3 import DRRMSAN_multiscale_attention_r2b
 from .DRRMSAN_BAYES import DRRMSAN_multiscale_attention_bayes
 from .UNet_AttnUNet_R2UNet_AttnR2UNet import unet, att_unet, r2_unet, att_r2_unet
 from .ModifiedUnet import ModifiedUNet
+from .drrmsan_001 import DRRMSAN_multiscale_attention_bayes_001
 
