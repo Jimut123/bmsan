151b111c46431ac42438a71374a41732de3cb11c
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-03-09 21:46:38 +0530

    k fold attn unet

--- SKIN_LEISON/ModifiedUNET_kFold/k_fold.py
+++ SKIN_LEISON/ModifiedUNET_kFold/k_fold.py
@@ -286,7 +286,7 @@ for train_index, test_index in kf.split(X):
 
         return model
     # img_w, img_h, n_label, data_format='channels_first'
-    model = ModifiedUNet(height=192, width=256, n_channels=3)
+    model = ModifiedUNet(height=256, width=192, n_channels=3)
 
     #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dice_coef, jacard, 'accuracy'])
     model.compile(optimizer=Adam(learning_rate=1e-5),loss='binary_crossentropy',metrics=[dice_coef, jacard, Recall(), Precision(), 'accuracy'])
