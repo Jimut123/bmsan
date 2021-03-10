fc896f737b29d692eff4cfaacb456d72edeb0bbf
vufyndr <gogol.goswami@gmail.com>
2021-03-10 18:25:50 +0530

    minor correction

--- LUNGS/AttnUNet_kFold/kfold.py
+++ LUNGS/AttnUNet_kFold/kfold.py
@@ -296,7 +296,7 @@ for train_index, test_index in kf.split(X):
 
         return model
     # img_w, img_h, n_label, data_format='channels_first'
-    model = att_unet(img_h=256, img_w=192, n_label=3)
+    model = att_unet(img_h=256, img_w=256, n_label=3)
 
     #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dice_coef, jacard, 'accuracy'])
     model.compile(optimizer=Adam(learning_rate=1e-5),loss='binary_crossentropy',metrics=[dice_coef, jacard, Recall(), Precision(), 'accuracy'])
