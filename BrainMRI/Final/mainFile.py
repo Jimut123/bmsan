71bb264d6e04a52400f1252933f97a559cbed89e
Jimut123 <jimutbahanpal@yahoo.com>
2021-03-23 08:42:49 +0530

    numpy to np

--- BrainMRI/Final/mainFile.py
+++ BrainMRI/Final/mainFile.py
@@ -314,7 +314,7 @@ def f(x):
     # to be maximized
     # dice_coef = drrmsan_multilosses.get_dice_from_alphas(float(alpha_1[0]), float(alpha_2[0]), float(alpha_3[0]), float(alpha_4[0]))
     # dice_coef =  float(alpha_1)+ float(alpha_2)+ float(alpha_3)+ float(alpha_4)
-    
+
     opt = tf.keras.optimizers.Nadam(LR)
     metrics = [dice_coef, jacard, Recall(), Precision() ,'accuracy']
     model.compile(loss=dice_loss, optimizer=opt, metrics=metrics)
@@ -469,7 +469,7 @@ def load_entire_file_into_memory_and_then_convert(filename):
     with open(filename, 'r') as input_file:
         full_file_contents = input_file.read()
         lines_of_file = full_file_contents.split('\n')
-        return numpy.array(lines_of_file)
+        return np.array(lines_of_file)
 
 dump = load_entire_file_into_memory_and_then_convert('store_alphas_dice.txt')
 print(dump)
@@ -481,10 +481,10 @@ for item in dump[:-1]:
     X.append([float(i) for i in all_items[:4]]) 
     Y.append(float(all_items[-2]))
 
-X = numpy.array(X)
+X = np.array(X)
 print(X)
 
-Y = -numpy.array(Y)
+Y = -np.array(Y)
 print(Y)
 
 Y = np.expand_dims(Y, axis=1)
