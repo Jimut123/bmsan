57d789397e6a1385636a01c3d83fbd9220faa583
Jimut123 <jimutbahanpal@yahoo.com>
2021-04-11 00:30:56 +0530

    no maxpool

--- models/drrmsan_022_attn_2.py
+++ models/drrmsan_022_attn_2.py
@@ -179,7 +179,7 @@ def attention_up_and_concate(down_layer, layer, filters):
     #concate = my_concat([down_layer, layer])
     return layer
 
-
+"""
 def proposed_attention_block_2d(ms_conv, res_block, filters):
     '''
     Proposed Attention block - Version 2 <|dream|> - 
@@ -206,8 +206,61 @@ def proposed_attention_block_2d(ms_conv, res_block, filters):
     attn_output_1 = multiply([up_attn, res_block])
     
     return attn_output_1
+"""
 
 
+def proposed_attention_block_2d(ms_conv, res_block, filters):
+    '''
+    Proposed Attention block - Version 2 <|dream-No-maxpool|> - 
+
+    Arguments:
+        ms_conv {keras layer} -- layer coming from the multi resolution convolution
+        res_block {keras layer} -- layer coming from the residual block
+        filters {int} -- number of channels in image
+
+    Returns:
+        [keras layer] -- [output layer]
+    '''
+
+    up_1 = Activation('relu')(Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(ms_conv))
+    up_2 = Activation('relu')(Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(up_1))
+
+    mult_block = Activation('sigmoid')(multiply([up_1, up_2]))
+
+    attn_output_1 = multiply([mult_block, res_block])
+    
+    return attn_output_1
+
+
+"""
+def proposed_attention_block_2d(ms_conv, res_block, filters):
+    '''
+    Proposed Attention block - Version 2 <|dream-2,2-maxpool|> - 
+
+    Arguments:
+        ms_conv {keras layer} -- layer coming from the multi resolution convolution
+        res_block {keras layer} -- layer coming from the residual block
+        filters {int} -- number of channels in image
+
+    Returns:
+        [keras layer] -- [output layer]
+    '''
+
+    up_1 = Activation('relu')(Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(ms_conv))
+    up_2 = Activation('relu')(Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(up_1))
+
+    down_1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(up_1)
+    down_2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(up_2)
+
+    mult_block = Activation('sigmoid')(multiply([down_1, down_2]))
+
+    up_attn = UpSampling2D(size=(2, 2))(mult_block)
+
+    attn_output_1 = multiply([up_attn, res_block])
+    
+    return attn_output_1
+
+"""
 
 
 
