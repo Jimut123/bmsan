80b7aeb5e02052013e42f1c100577e3ff5772d94
Jimut123 <jimutbahanpal@yahoo.com>
2021-04-11 20:18:52 +0530

    added

--- SKIN_LEISON/_KFOLD_FINAL/kfold.py
+++ SKIN_LEISON/_KFOLD_FINAL/kfold.py
@@ -38,7 +38,7 @@ from sklearn.metrics import average_precision_score, recall_score
 
 import sys
 sys.path.insert(0, '../../')
-from models import DRRMSAN_multiscale_attention_bayes_022
+from models import DRRMSAN_multiscale_attention_bayes_022_conc
 
 
 img_files = glob.glob('../trainx/*.bmp')
@@ -259,11 +259,11 @@ for train_index, test_index in kf.split(X):
 
         return model
     # img_w, img_h, n_label, data_format='channels_first'
-    alpha_1 = 0.3
-    alpha_2 = 0.07
-    alpha_3 = 0.32
-    alpha_4 = 0.3
-    model = DRRMSAN_multiscale_attention_bayes_022(height=192, width=256, n_channels=3, alpha_1 = alpha_1, alpha_2 = alpha_2, alpha_3 = alpha_3, alpha_4 = alpha_4)
+    alpha_1 = 0.28045761   
+    alpha_2 = 0.45257109
+    alpha_3 = 0.14933148
+    alpha_4 = 0.11764978
+    model = DRRMSAN_multiscale_attention_bayes_022_conc(height=192, width=256, n_channels=3, alpha_1 = alpha_1, alpha_2 = alpha_2, alpha_3 = alpha_3, alpha_4 = alpha_4)
     
 
 
