dc25a0569508dac0496245eef915cd69be43f441
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-04-12 21:52:46 +0530

    .gitignore

--- NERVE/MSAN_3/kfold.py
+++ NERVE/MSAN_3/kfold.py
@@ -84,8 +84,6 @@ fold_no = 0
 
 for train_index, test_index in kf.split(X):
     fold_no += 1
-    if fold_no <= 2:
-        continue
     # print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = X[train_index], X[test_index]
     Y_train, Y_test = Y[train_index], Y[test_index]
