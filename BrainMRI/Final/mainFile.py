71bb0b60c04fa0b69e181c151fe9a8a7adf49d0a
Jimut <jimutbahanpal@yahoo.com>
2021-05-06 20:06:02 +0530

    brain mri gd added

--- BrainMRI/Final/mainFile.py
+++ BrainMRI/Final/mainFile.py
@@ -72,7 +72,7 @@ tf.random.set_seed(42)
 ## Hyperparameters
 
 #IMG_SIZE = 256
-EPOCHS = 1
+EPOCHS = 100
 BATCH = 2
 LR = 1e-5
 
@@ -502,7 +502,7 @@ print(Y)
 Y = np.expand_dims(Y, axis=1)
 Y
 
-maxiter = 1
+maxiter = 40
 
 kernel = GPy.kern.Matern52(input_dim=4, ARD=True, variance=1, lengthscale=[1,1,1,1]);
 
